b6f2de6cdfad4ded2d26008ff5489090d3d1fdd4
Csörgő Márk Ábrahám <h153041@stud.u-szeged.hu>
2024-12-10 09:56:24 +0100

    api key oops

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,16 +7,7 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 import {getAuth, provideAuth} from '@angular/fire/auth';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBkfyeVrrqKBrMtS5np6kSOmcNFPWsLvQE",
-  authDomain: "pwa-projekt-interest.firebaseapp.com",
-  projectId: "pwa-projekt-interest",
-  storageBucket: "pwa-projekt-interest.firebasestorage.app",
-  messagingSenderId: "1080260484037",
-  appId: "1:1080260484037:web:83875a1c95cc0997f209ac",
-  measurementId: "G-J1PRBR7XYY"
-};
+import { firebaseConfig } from '../env/env';
 
 
 export const appConfig: ApplicationConfig = {
